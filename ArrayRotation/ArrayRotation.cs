d2d7e51b2dcf368d3f6d37cfbd1417ddad4fa18f
StanM84 <stan1984@abv.bg>
2023-10-20 17:24:47 +0300

    Add project files.

---
+++ ArrayRotation/ArrayRotation.cs
@@ -0,0 +1,12 @@
+﻿List<int> input = Console.ReadLine().Split( ).Select(int.Parse).ToList();
+
+int rotations = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < rotations; i++)
+{
+    int curentNum = input[0];
+
+    input.RemoveAt(0);
+    input.Add(curentNum);
+}
+Console.WriteLine(string.Join(" ", input));
---
+++ MergingLists/MergingLists.cs
@@ -0,0 +1,20 @@
+﻿List<int> input1 = Console.ReadLine().Split().Select(int.Parse).ToList();
+List<int> input2 = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+List<int> output = new List<int>();
+
+int longerCout = Math.Max(input1.Count, input2.Count);
+
+for (int i = 0; i < longerCout; i++)
+{
+    if (i < input1.Count)
+    {
+        output.Add(input1[i]);
+    }
+
+    if (i < input2.Count)
+    {
+        output.Add(input2[i]);
+    }
+}
+Console.WriteLine(string.Join(" ",output));
